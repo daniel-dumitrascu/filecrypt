92c20eb444eeeee4f7ec7f556f3e2b131e7d4b8b
Daniel Dumitrascu <daniel.dumitrascu.dev@gmail.com>
2024-03-13 15:12:46 +0200

    remove debug info

--- server/env/env.go
+++ server/env/env.go
@@ -22,7 +22,7 @@ type Environment struct {
 
 func (env *Environment) Setup() {
 	log := utils.GetLogger()
-	log.Info("<DEBUG> Setup the environment")
+	log.Info("Setup the environment")
 	osmanager := GetOsManager()
 	osmanager.SpecificSetup()
 	var installKeyPath = GetKeysDirPath()

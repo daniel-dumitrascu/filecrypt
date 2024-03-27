83d0de99b32881d3f02ae4def68bb8c0739d3a80
Daniel Dumitrascu <daniel.dumitrascu.dev@gmail.com>
2024-03-27 22:08:11 +0200

    revert debug info

--- client/client.go
+++ client/client.go
@@ -10,22 +10,13 @@ import (
 )
 
 func main() {
-	f, _ := os.Create("C:\\Users\\DanielDumitrascu\\Desktop\\log\\log.txt")
-	f.Write([]byte("in main()" + "\n"))
 	arguments := os.Args
-	//arguments := []string{"", "encrypt", "C:\\Users\\DanielDumitrascu\\Desktop\\1"}
-
 	if len(arguments) < 3 {
 		log.Fatalf("Exiting - action and path argument weren't provided")
 	}
 
 	action := arguments[1]
 	targetPath := string(arguments[2])
-
-	f.Write([]byte("Action: " + action + "\n"))
-	f.Write([]byte("Path: " + targetPath + "\n"))
-	f.Write([]byte("---------------------------------" + "\n"))
-	f.Close()
 
 	var reqData RequestData
 	if action == "encrypt" {
